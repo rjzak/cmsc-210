4e313a95c137f123ab381eb89e68afecd19c868f
James Stevenson <james.m.stevenson@gmail.com>
2020-08-03 23:29:13 -0400

    Add lecture 6 and assignment 1

--- cmsc_291/slides/template.py
+++ cmsc_291/slides/template.py
@@ -3,24 +3,22 @@ from pathlib import Path
 
 from .build import build_directory
 
-def template_dir_path() -> str:
-    return str(Path(__file__).parent / "templates")
+def template_dir_path() -> Path:
+    return Path(__file__).parent / "templates"
 
 
 env = Environment(
-    loader=FileSystemLoader(template_dir_path()),
+    loader=FileSystemLoader(str(template_dir_path())),
     autoescape=select_autoescape(['html', 'xml'])
 )
 
-
-SOURCES = ("index.html", "calendar.html", "lecture_01.html", "lecture_02.html", "lecture_03.html", "lecture_04.html", "lecture_05.html", "lecture_06.html")
-
 def build_index(location: str = "build") -> None:
-    for source in SOURCES:
-        template = env.get_template(source)
-        with build_directory(location) as build_dir:
-            with open(build_dir / source, "w") as fh:
-                html = template.render()
-                fh.write(html)
+    with build_directory(location) as build_dir:
+        for source in template_dir_path().iterdir():
+            if source.suffix == ".html":
+                template = env.get_template(source.name)
+                with open(build_dir / source.name, "w") as fh:
+                    html = template.render()
+                    fh.write(html)
 
 
