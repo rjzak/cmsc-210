86c410cc94c8f6acf58737d1d70b29cd628e7369
James Stevenson <james.m.stevenson@gmail.com>
2020-09-16 09:43:08 -0400

    Remove pre/postconditions stuff from coding standards.

--- cmsc_291/examples/lecture_05/robo_pet.py
+++ cmsc_291/examples/lecture_05/robo_pet.py
@@ -1,3 +1,6 @@
+import sys
+
+
 class Animal:
 
     def __init__(self, age, weight, name, breed):
@@ -8,9 +11,9 @@ class Animal:
 
     def feed(self, scoops=1):
         print(f"Feeding {self.name}...")
-    
+
     def groom(self):
-        raise NotImplementedError("Must implement this method on the child object.")
+        print("Bruching")
 
 
 class Cat(Animal):
@@ -18,24 +21,62 @@ class Cat(Animal):
     def change_litter(self):
         print(f"Changing litter for {self.name}...")
 
-    def groom(self):
-        print(f"Brushing {self.name}...")
 
-
-class Dog(Animal):
+class Dog:
 
     def __init__(self, age, weight, name, breed, housebroken=True):
+        self.animal = Animal(age, weight, name, breed)
         super().__init__(age, weight, name, breed)
         self.housebroken = housebroken
-    
+
     def groom(self):
-        print(f"Washing and drying {self.name}...")
+        print("Wash")
+        super().groom()
 
     def walk(self):
         print(f"Walking {self.name}...")
 
 
+
 class Pen:
 
     def __init__(self):
-        pass
+        self.pen = []
+
+    def add(self, animal):
+        if len(self.pen) == 4:
+            print("Pen is full.")
+        else:
+            self.pen.append(animal)
+
+    def remove(self, name):
+        for animal in self.pen:
+            if animal.name == name:
+                return  animal
+        print("Animal not found.")
+
+    def feed(self):
+        for animal in self.pen:
+            animal.feed()
+
+    def groom(self):
+        for animal in self.pen:
+            animal.groom()
+
+    def walk(self):
+        for animal in self.pen:
+            if isinstance(animal, Dog):
+                animal.walk()
+
+
+def read_csv(filename):
+    print(f"File is {filename}")
+
+
+if __name__ == "__main__":
+    import csv
+    filename = sys.argv[1]
+    read_csv(filename)
+    with open(filename) as filehandle:
+        for row in csv.reader(filehandle):
+           pass
