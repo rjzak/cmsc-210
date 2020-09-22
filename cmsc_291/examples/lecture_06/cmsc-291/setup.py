cee227160055beff152f042b3c374cd0147094cb
James Stevenson <james.m.stevenson@gmail.com>
2020-09-22 17:19:09 -0400

    Python packaging lecture

---
+++ cmsc_291/examples/lecture_06/cmsc-291/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cmsc-291',
+    version='1.0.0',
+    url='https://github.com/umbc/cmsc291.git',
+    author='James Stevenson',
+    author_email='author@gmail.com',
+    description='All code written in CMSC-291',
+    packages=["cmsc291"],
+    install_requires=['click == 7.1.2'  ],
+)
