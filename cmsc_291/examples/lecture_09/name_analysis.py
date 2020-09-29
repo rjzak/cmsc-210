c13f70e84baeed5d93d4633fde75020683f5b9c9
James Stevenson <james.m.stevenson@gmail.com>
2020-09-29 18:46:54 -0400

    Add name analysis code.

--- cmsc_291/examples/lecture_09/name_analysis.py
+++ cmsc_291/examples/lecture_09/name_analysis.py
@@ -1,5 +1,5 @@
 import csv
-import seaborn as sns
+import seaborn
 
 """
 In a given year:
@@ -13,3 +13,56 @@ Across a period of years?
     * How do we measure "popular," anyway?
 
 """
+
+
+def get_year(year):
+    filename = f"names/yob{year}.txt"
+    female_data = []
+    male_data = []
+    with open(filename) as fh:
+        for row in csv.DictReader(fh, fieldnames=("name", "sex", "count")):
+            if row["sex"] == "F":
+                female_data.append({"name": row["name"], "count": int(row["count"])})
+            else:
+                male_data.append({"name": row["name"], "count": int(row["count"])})
+    return female_data, male_data
+
+
+def get_count(row):
+    return row["count"]
+
+
+def most_popular(data):
+    sorted_names = sorted(data, key=get_count, reverse=True)
+    return sorted_names[0]
+
+
+def rank(data, name):
+    total = 0
+    name_count = 0
+    for counter, row in enumerate(data):
+        total += row["count"]
+        if row["name"] == name:
+            name_count = row["count"]
+    return (name_count / total) * 100
+
+
+def popularity(name, sex):
+    series = []
+    for year in range(1880, 2019):
+        female, male = get_year(year)
+        if sex == "F":
+            series.append({"year": year, "rank": rank(female, name)})
+        else:
+            series.append({"year": year, "rank": rank(male, name)})
+    return series
+
+
+def chart_popularity(name, sex):
+    x = []
+    y = []
+    series = popularity(name, sex)
+    for row in series:
+        x.append(row["year"])
+        y.append(row["rank"])
+    seaborn.relplot(x=x, y=y, kind="line")
