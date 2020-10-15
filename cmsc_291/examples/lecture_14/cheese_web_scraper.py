0b467632f096800be86ea42bbf7466b60f1d3a7e
James Stevenson <james.m.stevenson@gmail.com>
2020-10-15 17:25:15 -0400

    Fix a typo.

--- cmsc_291/examples/lecture_14/cheese_web_scraper.py
+++ cmsc_291/examples/lecture_14/cheese_web_scraper.py
@@ -3,24 +3,3 @@ import requests
 
 URL = "https://cheese.com/bleu-dauvergne/"
 
-HTML = """
-<ul>
-    <li>This is <a href="http://example.com">link number 1</a>.</li>
-    <li id="foo">This is <a href="http://example.com">link number 2</a>.</li>
-</ul>
-"""
-
-
-def get_soup(url):
-    response = requests.get(url)
-    return BeautifulSoup(response.text, 'html.parser')
-
-
-def cheese_properties(soup):
-    attributes = {}
-    for li in soup.select("ul.summary-points li p"):
-        list_item = li.text
-        if ":" in list_item:
-            attribute, value = list_item.split(":")
-            attributes[attribute.strip()] = value.strip()
-    return attributes
