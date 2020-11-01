5d15aaf93de4cd70515b004ef8ebeb753a7f0cd7
James Stevenson <james.m.stevenson@gmail.com>
2020-11-01 18:46:38 -0500

    Lecture 19

---
+++ cmsc_291/examples/lecture_19/cheeseshop/cheeseshop/cheeses.py
@@ -0,0 +1,6 @@
+CHEESES = {
+	"brie": ("Brie", "France", "Brie is a soft cow's-milk cheese named after Brie, the French region from which it originated."),
+	"valdeon": ("Valdeón", "Spain", " A Spanish blue cheese from León. It is wrapped in sycamore leaves before being sent to market."),
+	"monterey-jack": ("Monterey Jack", "USA", "An American white, semi-hard cheese made using cow's milk. It is noted for its mild flavor and slight sweetness."),
+	"brillat-savarin": ("Brillat-Savarin", "France", "A soft-ripened triple cream cow's milk cheese with at least 72% fat. Made in the province of Normandy.")
+}
---
+++ cmsc_291/examples/lecture_19/cheeseshop/cheeseshop/views.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+from django.http import HttpResponseNotFound, HttpResponseRedirect
+from django.shortcuts import render
+
+from cheeseshop.cheeses import CHEESES
+
+
+def index(request):
+    now = datetime.now()
+    return render(request, "index.html", {"now": now})
+
+
+def cheese_list(request):
+    cheeses = sorted([(slug, name, country) for slug, (name, country, _) in CHEESES.items()])
+    return render(request, "cheese_list.html", {"cheeses": cheeses})
+
+
+def cheese_detail(request, cheese_id):
+    if cheese_id not in CHEESES:
+        return HttpResponseNotFound(f"I don't know about a cheese called {cheese_id}.")
+    name, country, description = CHEESES[cheese_id]
+    return render(request, "cheese_detail.html", {"name": name, "country": country, "description": description})
