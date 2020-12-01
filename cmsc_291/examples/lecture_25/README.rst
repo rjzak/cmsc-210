bd7f68b0d53c10ba061cfa0db2d0f9a5edfcf5e6
James Stevenson <james.m.stevenson@gmail.com>
2020-11-30 23:34:07 -0500

    Lecture 25

---
+++ cmsc_291/examples/lecture_25/README.rst
@@ -0,0 +1,13 @@
+Baltimore City 311 Request Data Exploration
+===========================================
+
+To get started, make sure you install these three libraries::
+
+    $> conda install numpy scikit-learn  pandas jupyterlab matplotlib
+
+
+Next you'll want to start up the `Jupyter Lab <https://jupyterlab.readthedocs.io/en/latest/>`_ webserver::
+
+    $> jupyter lab --config="jupyter_notebook_config.py"
+
+When you do this, JupyterLab will open automatically in your browser.
---
+++ cmsc_291/examples/lecture_25/jupyter_notebook_config.py
@@ -0,0 +1,20 @@
+#------------------------------------------------------------------------------
+# CMSC 291 Lecture 10: Default Application configuration for Jupyter Lab
+#------------------------------------------------------------------------------
+
+
+## The directory to use for notebooks and kernels.
+c.NotebookApp.notebook_dir = 'notebooks'
+
+## Forces users to use a password for the Notebook server. This is useful in a
+#  multi user environment, for instance when everybody in the LAN can access each
+#  other's machine through ssh.
+#
+#  In such a case, serving the notebook server on localhost is not secure since
+#  any user can connect to the notebook server via ssh. But not necessary here.
+c.NotebookApp.password_required = False
+
+## The port the notebook server will listen on (env: JUPYTER_PORT).
+c.NotebookApp.port = 8888
+
+
