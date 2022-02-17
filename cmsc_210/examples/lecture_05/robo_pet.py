fa87df1bfb14c5c7d89b1ca79d2aca8a92e78276
James Stevenson <james.m.stevenson@gmail.com>
2022-02-16 23:25:45 -0500

    Lecture 5 final updates.

--- cmsc_210/examples/lecture_05/robo_pet.py
+++ cmsc_210/examples/lecture_05/robo_pet.py
@@ -1,6 +1,3 @@
-import sys
-
-
 class Animal:
     def __init__(self, age, weight, name, breed):
         self.age = age
@@ -8,21 +5,21 @@ class Animal:
         self.name = name
         self.breed = breed
 
-    def feed(self, scoops=1):
+    def feed(self):
         print(f"Feeding {self.name}...")
 
     def groom(self):
-        print("Bruching")
+        print("Brushing")
 
 
 class Cat(Animal):
+
     def change_litter(self):
         print(f"Changing litter for {self.name}...")
 
 
-class Dog:
+class Dog(Animal):
     def __init__(self, age, weight, name, breed, housebroken=True):
-        self.animal = Animal(age, weight, name, breed)
         super().__init__(age, weight, name, breed)
         self.housebroken = housebroken
 
@@ -35,44 +32,21 @@ class Dog:
 
 
 class Pen:
+
     def __init__(self):
-        self.pen = []
+        pass
 
     def add(self, animal):
-        if len(self.pen) == 4:
-            print("Pen is full.")
-        else:
-            self.pen.append(animal)
+        pass
 
     def remove(self, name):
-        for animal in self.pen:
-            if animal.name == name:
-                return animal
-        print("Animal not found.")
+        pass
 
     def feed(self):
-        for animal in self.pen:
-            animal.feed()
+        pass
 
     def groom(self):
-        for animal in self.pen:
-            animal.groom()
+        pass
 
     def walk(self):
-        for animal in self.pen:
-            if isinstance(animal, Dog):
-                animal.walk()
-
-
-def read_csv(filename):
-    print(f"File is {filename}")
-
-
-if __name__ == "__main__":
-    import csv
-
-    filename = sys.argv[1]
-    read_csv(filename)
-    with open(filename) as filehandle:
-        for row in csv.reader(filehandle):
-            pass
+        pass
