db15356150fa609aa774d66859115d6b9f2a4de2
James Stevenson <james.m.stevenson@gmail.com>
2022-02-23 22:48:53 -0500

    Update packaging lecture.

--- cmsc_210/examples/lecture_06/cmsc-210/setup.py
+++ cmsc_210/examples/lecture_06/cmsc-210/setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(
     name="cmsc-210",
     version="1.0.0",
-    url="https://github.com/umbc/cmsc291.git",
+    url="https://github.com/mazelife/cmsc-210",
     author="James Stevenson",
     author_email="author@gmail.com",
     description="All code written in CMSC-210",
     packages=["cmsc210"],
-    install_requires=["click == 7.1.2"],
+    install_requires=["click == 8.0.4"],
 )
